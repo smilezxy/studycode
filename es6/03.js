7f7711bd290ba05526809d5b84e728b10f38ad21
zxy <123445@qq.com>
2018-07-31 09:48:41 +0800

    学习

---
+++ es6/03.js
@@ -0,0 +1,23 @@
+/*
+    字符串相关扩展
+    includes()  判断字符串中是否包含指定的字符串   返回true  false
+                参数一：匹配的字符串  参数二：从第几个字符开始匹配
+    startsWith()  判断字符串是否以特定的字符串开始
+    endsWith()  判断字符串是否以特定的字符串结束
+*/
+console.log('hello world'.includes('world'))   // true
+console.log('hello world'.includes('world',7)) // false
+let url = 'admin/login'
+console.log(url.startsWith('admin'))  // true
+console.log(url.endsWith('php'))
+
+//------------------------------------
+
+/* 模板字符串 */
+let obj = {
+    username:'zxy',
+    age:'24'
+}
+let tag = '<div><span>'+ obj.username +'</span></div>'
+
+let tpl = ``
